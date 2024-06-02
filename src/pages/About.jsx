fa3f1d14106216757101ed881936dbe3d1ef7d32
Nurudeen Abdul-Majeede <abdulmajeednurudeen48@gmail.com>
2024-06-02 11:47:51 -0800

    Added all the elements of the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,11 @@
 import NavBar from '../components/NavBar';
 import Socials from '../components/Socials';
 import Footer from '../components/Footer';
+import ProductsCarousel from '../components/ProductsCarousal';
 
 import backgroundImage from '/src/assets/about-bg.jpg';
+import aboutPhoto1 from '/src/assets/library-of-congress-lw6pO_m2Ve4-unsplash.jpg';
+import aboutPhoto2 from '/src/assets/austrian-national-library-DSrf3mapye4-unsplash.jpg';
 
 const About = () => {
   return (
@@ -25,7 +28,109 @@ const About = () => {
             </p>
           </div>
         </div>
-        <h1>About Page</h1>
+
+        <div>
+          <div>
+            <h2>OUR STORY</h2>
+
+            <div>
+              <div>
+                <h1>Since 1928</h1>
+
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+                  libero massa, bibendum sit amet facilisis non, ullamcorper non
+                  est. Suspendisse potenti. Integer a mi vel purus mollis
+                  faucibus porttitor nec libero. Phasellus eget lacinia arcu.
+                  Quisque ac enim accumsan, tristique tellus eu, accumsan erat.
+                  Nam dictum ornare sagittis.
+                </p>
+
+                <p>
+                  Ut faucibus sollicitudin est bibendum condimentum. Sed sit
+                  amet urna nec diam iaculis pretium quis vel neque.
+                  Pellentesque vitae mattis eros, a accumsan nibh. Sed gravida
+                  quam ultricies purus dictum hendrerit. Curabitur et tincidunt
+                  felis.
+                </p>
+              </div>
+
+              <div>
+                <img src={aboutPhoto1} alt="about photo" />
+
+                <img src={aboutPhoto2} alt="different about photo" />
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <div>
+                <p>Since 1928</p>
+
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+                  libero massa, bibendum sit amet facilisis non, ullamcorper non
+                  est. Suspendisse potenti. Integer a mi vel purus mollis
+                  faucibus porttitor nec libero. Phasellus eget lacinia arcu.
+                </p>
+              </div>
+            </div>
+
+            <div>
+              <ProductsCarousel />
+            </div>
+
+            <div>
+              <div>
+                <div>
+                  <h2>Core Values</h2>
+
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Morbi libero massa, bibendum sit amet facilisis non,
+                    ullamcorper non est. Suspendisse potenti. Integer a mi vel
+                    purus mollis faucibus porttitor nec libero. Phasellus eget
+                    lacinia arcu. Quisque ac enim accumsan, tristique tellus eu,
+                    accumsan erat. Nam dictum ornare sagittis. Ut faucibus
+                    sollicitudin est bibendum condimentum. Sed sit amet urna nec
+                    diam iaculis pretium quis vel neque. Pellentesque vitae
+                    mattis eros, a accumsan nibh. Sed gravida quam ultricies
+                    purus dictum hendrerit. Curabitur et tincidunt felis.
+                  </p>
+                </div>
+
+                <div>
+                  <h2>Our Community</h2>
+
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Morbi libero massa, bibendum sit amet facilisis non,
+                    ullamcorper non est. Suspendisse potenti. Integer a mi vel
+                    purus mollis faucibus porttitor nec libero. Phasellus eget
+                    lacinia arcu. Quisque ac enim accumsan, tristique tellus eu,
+                    accumsan erat. Nam dictum ornare sagittis. Ut faucibus
+                    sollicitudin est bibendum condimentum. Sed sit amet urna nec
+                    diam iaculis pretium quis vel neque. Pellentesque vitae
+                    mattis eros, a accumsan nibh. Sed gravida quam ultricies
+                    purus dictum hendrerit. Curabitur et tincidunt felis.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <q>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+            tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+          </q>
+
+          <p>Nurudeen Abdul-Majeed</p>
+
+          <p>Developer</p>
+        </div>
       </main>
       <Footer />
     </div>
